ff0f594bbfdf5077491ab7579a95251a3b415377
ifiokInyang <80972231+ifiokInyang@users.noreply.github.com>
2022-09-12 14:58:27 +0100

    Update index.js
    Solution for the transpose challenge

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,20 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+let finalMatrixArray = []
+for (let i = 0; i<array[0].length; i++){
+  
+  let firstItem = array[0][i];
+  let secondItem = array[1][i];
+  let thirdItem = array[2][i];
+  let newTwoDArray = [firstItem, secondItem, thirdItem]
+  finalMatrixArray.push(newTwoDArray)
+} 
+return finalMatrixArray;
+
+}
+
+console.log(transpose([[5, 12, 17, 9, 3],[13, 4, 8, 14, 1],[9, 6, 3, 7, 21]]))
 
 module.exports = transpose;
